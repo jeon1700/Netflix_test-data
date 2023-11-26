95a24a591c4dc0e66c73d584164573151db9043e
jeon1700 <151480745+jeon1700@users.noreply.github.com>
2023-11-27 04:58:14 +0900

    main

--- app.py
+++ app.py
@@ -3,12 +3,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from app_home import home_app
+
 
 
 
 def main():
-    pass
+    st.title('Netflix 추천영화')
 
+    menu = ['Home','Choice','Data']
+
+    choice = st.sidebar.selectbox('메뉴',menu)
+
+    if choice == menu[0] :
+        home_app()
+    elif choice == menu[1] :
+        pass
+    elif choice == menu[2] :
+        pass
 
 if __name__=='__main__' :
     main()
---
+++ app_home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def home_app():
+    st.text('*해당 사이트는 Netflix Movie & documentary를 추천합니다.*')
+    st.subheader('')
+
+    st.info('AWS에 배포까지 된 앱입니다.')
