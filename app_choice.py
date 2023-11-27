e3c2fc26bb61ee79fa00bfe399f4ce15c91a0a09
jeon1700 <151480745+jeon1700@users.noreply.github.com>
2023-11-27 23:57:37 +0900

    main

--- app_choice.py
+++ app_choice.py
@@ -1,38 +1,38 @@
 import streamlit as st
 import numpy as np
-import joblib
 import pandas as pd
 
 
 
 
-def choice_app() :
+import streamlit as st
+import pandas as pd
+
+def choice_app():
     st.subheader('추천장르 찾기')
 
-    netfl = joblib.load('./model/netfl.pkl')
-    type = st.radio('Choice', ['영화','다큐멘터리'])
-    if type == '영화' :
-        type = 0
-    else :
-        type = 1
+    netfl = pd.read_csv('./data/imdb_movies_shows.csv')
 
-    runtime = st.slider('상영 시간', 30, 60, (30, 60), step=1)
-    imdb_score = st.slider('종합 점수', 5.0, 8.0, (5.0, 8.0), step=0.1)
-    imdb_votes = st.slider('시청자 추천', 0, 100000, (0, 100000), step=1000)
+    # 컬럼 이름을 소문자로 변환
+    netfl.columns = netfl.columns.str.lower()
 
-    runtime_start, runtime_end = runtime
-    imdb_score_start, imdb_score_end = imdb_score
-    imdb_votes_start, imdb_votes_end = imdb_votes
+    # 선택 옵션
+    type_option = st.radio('Choice', ['영화', '다큐멘터리'])
+    runtime_option = st.slider('상영 시간', min_value=0, max_value=300, step=1, value=(0, 300))
+    imdb_score_option = st.slider('종합 점수', min_value=0.0, max_value=10.0, step=0.1, value=(0.0, 10.0))
+    imdb_votes_option = st.slider('시청자 추천', min_value=0, max_value=1000000, step=1000, value=(0, 1000000))
 
-    if st.button('추천 장르') :
-        new_data = np.array([type, runtime_start, runtime_end, imdb_score_start, imdb_score_end, imdb_votes_start, imdb_votes_end])
-        print(new_data)
-        new_data = new_data.reshape(1, -1)
-        y_pred = netfl.predict(new_data)
-        st.text( y_pred )
-        price = y_pred[0]
+    # 조건에 맞는 데이터 필터링
+    filtered_data = netfl[
+        (netfl['type'].str.lower() == type_option) &
+        (netfl['runtime'] >= runtime_option[0]) & (netfl['runtime'] <= runtime_option[1]) &
+        (netfl['imdb_score'] >= imdb_score_option[0]) & (netfl['imdb_score'] <= imdb_score_option[1]) &
+        (netfl['imdb_votes'] >= imdb_votes_option[0]) & (netfl['imdb_votes'] <= imdb_votes_option[1])
+    ]
 
-        if price > 5 :
-            st.text('이 장르를 추천합니다.')
-        else:
-            st.text('죄송합니다. 현재 추천 장르가 없습니다.')
+    # 결과 표시
+    if not filtered_data.empty:
+        st.write('추천 영화 목록:')
+        st.dataframe(filtered_data)
+    else:
+        st.write('추천 영화가 없습니다.')
